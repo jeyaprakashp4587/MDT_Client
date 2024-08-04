4ffc842e1a0da5cc81cd9de2080bfca79407e9fe
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-08-04 20:08:26 +0530

    contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "../styles/contact.css";
-import { TextField } from "@mui/material";
+import { Divider, Paper, TextField } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faMobile,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
   return (
@@ -10,7 +17,24 @@ const Contact = () => {
         <TextField id="outlined-basic" label="Full Name" variant="outlined" />
         <TextField id="outlined-basic" label="Email" variant="outlined" />
         <TextField id="outlined-basic" label="Message" variant="outlined" />
-        <button>submit</button>
+        <button>
+          <FontAwesomeIcon icon={faEnvelope} /> submit
+        </button>
+      </div>
+      <Divider textAlign="center">OR</Divider>
+      <div className="contact-options">
+        <Paper className="contact-option bg-dark" style={{ color: "white" }}>
+          <FontAwesomeIcon icon={faPhone} />
+          +91 9025167302
+        </Paper>
+        <Paper className="contact-option bg-primary" style={{ color: "white" }}>
+          <FontAwesomeIcon icon={faEnvelope} />
+          mdtlib.Gmail.com
+        </Paper>
+        <Paper className="contact-option bg-danger" style={{ color: "white" }}>
+          <FontAwesomeIcon icon={faWhatsapp} />
+          WhatsApp
+        </Paper>
       </div>
     </div>
   );
